f1f042f5a89fc78de2cfc6b5ac93839ea1cd3f59
SAMO-PC <xjakubecs@stuba.sk>
2022-02-17 12:17:19 +0100

    hello world

--- src/main/java/sk/stuba/fei/uim/oop/AP.java
+++ src/main/java/sk/stuba/fei/uim/oop/AP.java
@@ -2,6 +2,19 @@ package sk.stuba.fei.uim.oop;
 
 public class AP {
     public static void main(String[] args) {
-        System.out.println("Hello world.");
+        int celeCislo = 10;
+        long vacsieCislo = 10L;
+        float desatinneCislo = 5.4f;
+        double vacsieDesatinneCislo = 5.4212;
+        char znak = 'b';
+        String retazecZnakov = "abc";
+        boolean bool = true;
+
+        int podiel = 2/3;
+        System.out.println(podiel);
+        int zvysok = 2%3;
+        System.out.println(zvysok);
+        float podielFloat = 2/3.0f;
+        System.out.println(podielFloat);
     }
 }
