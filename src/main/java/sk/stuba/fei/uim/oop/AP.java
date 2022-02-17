784f006593b6e4511414c25b7a7e645d87b50974
SAMO-PC <xjakubecs@stuba.sk>
2022-02-17 12:34:35 +0100

    pole, for

--- src/main/java/sk/stuba/fei/uim/oop/AP.java
+++ src/main/java/sk/stuba/fei/uim/oop/AP.java
@@ -2,19 +2,15 @@ package sk.stuba.fei.uim.oop;
 
 public class AP {
     public static void main(String[] args) {
-        int celeCislo = 10;
-        long vacsieCislo = 10L;
-        float desatinneCislo = 5.4f;
-        double vacsieDesatinneCislo = 5.4212;
-        char znak = 'b';
-        String retazecZnakov = "abc";
-        boolean bool = true;
-
-        int podiel = 2/3;
-        System.out.println(podiel);
-        int zvysok = 2%3;
-        System.out.println(zvysok);
-        float podielFloat = 2/3.0f;
-        System.out.println(podielFloat);
+        int[] pole = new int[10];
+        for(int i = 0; i < pole.length; i++){
+            pole[i] = i;
+        }
+        for(int i = 0; i < pole.length; i++){
+            AP.vypis(i);
+        }
+    }
+    public static void vypis(int x){
+        System.out.println(x);
     }
 }
